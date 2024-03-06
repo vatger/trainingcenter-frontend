f3edab12ad4f087e78e52b85469cf4b966510d28
Nikolas Görlitz <git@ngoerlitz.de>
2024-03-06 19:27:23 +0100

    Fix env/config issue

--- src/core/Config.ts
+++ src/core/Config.ts
@@ -12,8 +12,8 @@ export const Config = {
     VERSION: "0.0.1-Beta",
     VATGER_BROWSER_TOKEN_NAME: "vatger_tc_browser_token",
 
-    APP_HOST: import.meta.env.ENV == "production" ? "https://api.tc.vatsim-germany.org" : "http://localhost:8000",
-    API_BASE_URL: import.meta.env.ENV == "production" ? "https://tc.vatsim-germany.org" : "http://localhost:8001",
+    APP_HOST: "https://tc-dev.vatsim-germany.org",
+    API_BASE_URL: "https://tc-api-dev.vatsim-germany.org",
     DATE_FORMAT: "DD.MM.YYYY",
     DATETIME_FORMAT: "DD.MM.YYYY HH:mm",
 
